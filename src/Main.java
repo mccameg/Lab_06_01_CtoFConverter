64fd19decce964a79eb66916a5378344f980c0ab
mccameg <meganmccarren@yahoo.com>
2023-02-19 18:36:06 -0500

    Working

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,28 @@
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args)
     {
        double fahrenheitTemp = 0;
        double celsiusTemp = 0;
-       
+       String trash = "";
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the temperature in Celsius: ");
+        if(in.hasNextDouble())
+        {
+            celsiusTemp = in.nextDouble();
+            in.nextLine(); // clears the buffer
+            fahrenheitTemp = celsiusTemp * 9/5 + 32;
+            System.out.println("The equivalent temperature in Fahrenheit is " + fahrenheitTemp + "degrees");
+        }
+        else
+        {
+            trash = in.nextLine();
+            System.out.println("Must enter a valid number for Celsius temperature: " + trash);
+        }
+
     }
 }
